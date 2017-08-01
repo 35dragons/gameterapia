0af79577f20617d58f05828ec186f4650b50adcb
35dragons <gabrielbatistacaldeira@hotmail.com>
2017-08-01 06:01:02 -0300

    56

--- pages/dbconec.php
+++ pages/dbconec.php
@@ -5,5 +5,8 @@ $host = "gameterapia.database.windows.net"; $user = "gameterapia"; $password = "
 sqlsrv_connect($host,$user,$password) or die("Falha na conexão com o Banco de Dados"); 
  
 sqlsrv_select_db($database) or die("Falha ao selecionar o database"); 
+
+if(sqlsrv_connect)
+	echo"Tudo certo!";
  
 ?>
